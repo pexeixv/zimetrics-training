1df477ea3666a216217094c80330518b222cf8d1
pexeixv <gavineditz@gmail.com>
2022-08-17 10:28:40 +0530

    portfolio items made filterable

--- css/js/script.js
+++ css/js/script.js
@@ -2,8 +2,6 @@ const $ = (q, e=document) => e.querySelector(q)
 const $$ = (q, e=document) => e.querySelectorAll(q)
 
 const scrollUp = $('#scroll_up')
-
-
 window.addEventListener('scroll', e=>{
     (window.scrollY > 600) ? scrollUp.classList.add('scroll_up-visible') : scrollUp.classList.remove('scroll_up-visible') 
 })
@@ -12,6 +10,32 @@ const aside = $('aside')
 const navMenuIcons = $$('#nav_menu i')
 
 const toggleAside= ()=>{
+    aside.classList.toggle('aside-shown')
+    navMenuIcons.forEach(icon=>icon.classList.toggle('hide'))
+}
+
+const projects = $$('.project')
+const filters = $$('.projects_filters')
+
+const filterProjects = that =>{
+    const filter = that.innerText.toLowerCase()
+    const filterButtons = $$('.projects_filter')
+    filterButtons.forEach(button=>button.classList.remove('projects_filter-focus'))
+    if (filter==='all'){
+        projects.forEach(project=>project.classList.remove('hide'))
+        that.classList.add('projects_filter-focus')
+        return filter
+    }
+    projects.forEach(project=>{
+        if($('img', project).dataset.filter === filter)
+            project.classList.remove('hide')
+        else project.classList.add('hide')
+        that.classList.add('projects_filter-focus')
+    })
+}
+
+
+const hideAside = () => {
     aside.classList.toggle('aside-shown')
     navMenuIcons.forEach(icon=>icon.classList.toggle('hide'))
 }
